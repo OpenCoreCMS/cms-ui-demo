1eb9844f96e56db8f1c923cf917cab788641a490
Vot <vot@siliconfen.co>
2020-08-30 00:48:40 +0100

    Restructure nav

--- components/AppTemplate.js
+++ components/AppTemplate.js
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
+import styles from './AppTemplate.module.css'
+
 export default function AppTemplate({
   children,
   title = 'OPP Demo',
@@ -14,24 +16,38 @@ export default function AppTemplate({
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <header>
+      <header className={styles.header}>
         <div className="maxWidthLimitedContainer">
-          <nav>
+          <nav className="navLeft">
             <span>
               <img src="/public/opp-logo-500px.png" alt="Open Publishing Platform logo" width="30" />
-              Open Publishing Platform
+              OPP Demo
             </span>
             {' | '}
             <Link href="/">
               <a>Home</a>
             </Link>
             {' | '}
-            <Link href="/journal">
+            <Link href="/subjects">
+              <a>Browse by subject</a>
+            </Link>
+            {' | '}
+            <Link href="/journals">
               <a>Journals</a>
+            </Link>
+            {' | '}
+            <Link href="/books">
+              <a>Books</a>
             </Link>
             {' | '}
             <Link href="/about">
               <a>About</a>
+            </Link>
+          </nav>
+
+          <nav className="navRight">
+            <Link href="/signin">
+              <a>Sign in</a>
             </Link>
           </nav>
         </div>
@@ -39,19 +55,29 @@ export default function AppTemplate({
 
       {children}
 
-      <footer className="footer">
+      <footer className={styles.footer}>
         <div className="maxWidthLimitedContainer">
-          <a href="https://OpenPublishingPlatform.com" target="_blank">
-            Powered by Open Publishing Platform
-          </a>
 
-          <a href="/legal/terms-of-use">
-            Terms of use
-          </a>
+          <div className={styles.row}>
+            <Link href="/legal/terms-of-use">
+              <a>
+              Terms of use
+              </a>
+            </Link>
+            {' | '}
+            <Link href="/legal/privacy-policy">
+              <a>
+              Privacy policy
+              </a>
+            </Link>
+          </div>
 
-          <a href="/legal/privacy-policy">
-            Privacy policy
-          </a>
+          <div className={styles.row}>
+            <a href="https://OpenPublishingPlatform.com" target="_blank">
+              Powered by Open Publishing Platform
+            </a>
+          </div>
+
         </div>
       </footer>
     </div>
