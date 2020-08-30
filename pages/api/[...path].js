a3fe5b9b76620e9e5987c1b66b4752b7b1c86d8c
Vot <vot@siliconfen.co>
2020-08-30 18:34:36 +0100

    Proxy API URLs

---
+++ pages/api/[...path].js
@@ -0,0 +1,30 @@
+// We proxy all requests to the dedicated BFF (Backend For Frontend)
+const axios = require('axios');
+const bffBaseUrl = 'http://localhost:4000/api';
+
+export default function handler(req, res) {
+  const urlSegments = req.query.path;
+
+  if (!Array.isArray(urlSegments) || !urlSegments.length) {
+    return res.status(500).send('Bad request');
+  }
+
+  const fullPath = urlSegments.join('/');
+  const fullUrl = `${bffBaseUrl}/${fullPath}`;
+  console.log(`Fetching the data from API (via UI app proxy): ${fullUrl}`);
+
+  let fullData;
+
+  return axios.get(fullUrl)
+  .then(function (response) {
+    fullData = response.data.data;
+  })
+  .catch(function (error) {
+    console.log(error);
+    return res.status(500).send('Bad response from API');
+  })
+  .then(function () {
+    return res.status(200).json(fullData);
+  });
+
+}
