470208480f4576b1eb209ddaa7683c4d02b0f777
Vot <vot@siliconfen.co>
2020-08-30 18:37:26 +0100

    Add a list of results component

---
+++ components/ListOfResults.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+import styles from './ListOfResults.module.css'
+
+export default function AppTemplate({
+  children,
+  data = [],
+}) {
+  const x = data.map((item) => (
+    <li className={styles.result}>
+      <Link href="/journals/journalId/article/[itemId]" as={`/journals/journalId/article/${item.id}`}>
+        <a>{item.title}</a>
+      </Link>
+      <br />
+      <span>Authors: {item.authorLine}</span><br />
+      <span>Type: {item.titlePrefix}</span><br />
+      <br />
+      <span>DOI: {item.doi}</span><br />
+      <span>{item.impactStatement}</span><br />
+    </li>
+  ))
+
+  return <ul className={styles.lor}>{x}</ul>;
+}
--- pages/journals/[journalId]/current-issue.js
+++ pages/journals/[journalId]/current-issue.js
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
 import AppTemplate from '../../../components/AppTemplate'
+import ListOfResults from '../../../components/ListOfResults'
 import styles from '../../../styles/Journal.module.css'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function JournalItemCurrentIssuePageRender() {
   const router = useRouter();
   const { journalId } = router.query;
+
+  const { data, error } = useSWR('/api/v1/getArticles', fetcher)
 
   return (
     <AppTemplate>
@@ -13,11 +20,12 @@ export default function JournalItemCurrentIssuePageRender() {
           <h1 className={styles.title}>
             Current issue of journal
           </h1>
-          <p>JournalID: {journalId} </p>
+          <p>Journal ID: {journalId} </p>
 
           <hr />
 
-          <p>Article list here</p>
+          <ListOfResults data={data}></ListOfResults>
+
         </div>
       </main>
     </AppTemplate>
