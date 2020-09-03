ef57d70d7e8f05a88b434eac547cb92356d3c960
Vot <vot@siliconfen.co>
2020-09-03 19:27:46 +0100

    Tweak panel spacing, make title markup optional

--- components/CommonElements/Panel.js
+++ components/CommonElements/Panel.js
@@ -2,10 +2,11 @@ import PanelStyles from '../CommonElements/Panel.module.css'
 
 export default function Panel({ children, collapsible = false, collapsed = false, title }) {
   return <div className={PanelStyles.panel}>
-    <div className={PanelStyles.panelTitle}>
+    {title ? <div className={PanelStyles.panelTitle}>
       {collapsible ? <span>&nabla; &Delta; </span> : <span></span>}
       <strong>{title}</strong>
-    </div>
+    </div> : ''}
+
     <div className={PanelStyles.panelBody}>
       {children}
     </div>
