7707803c213e0c8401d124ad523d46152442bb3a
Vot <vot@siliconfen.co>
2021-02-22 22:22:48 +0000

    Fix paths

--- components/AppTemplate/AppTemplate.js
+++ components/AppTemplate/AppTemplate.js
@@ -18,7 +18,7 @@ export default function AppTemplateComponent({
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/public/OpenCoreCMSIcon-64.png" />
       </Head>
 
       <AppHeaderForPrint></AppHeaderForPrint>
