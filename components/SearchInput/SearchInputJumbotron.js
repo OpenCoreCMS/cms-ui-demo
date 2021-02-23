dd8290c75324f2d16487e68bd94d325a4a98c254
Vot <vot@siliconfen.co>
2021-02-23 00:37:50 +0000

    More style updates

--- components/SearchInput/SearchInputJumbotron.js
+++ components/SearchInput/SearchInputJumbotron.js
@@ -3,7 +3,7 @@ import SearchInputStyles from './SearchInput.module.css'
 export default function SearchInputJumbotronComponent() {
   return <form action="/search">
     <div className={SearchInputStyles.jumbotronSearchContainer}>
-      <input className={SearchInputStyles.jumbotronSearchInput} type="search" name="phrase" placeholder="Search Open Access content" />
+      <input className={SearchInputStyles.jumbotronSearchInput} type="search" name="phrase" placeholder="Start discovering here" />
       <input className={SearchInputStyles.jumbotronSearchSubmit} type="submit" value="Run search" />
     </div>
   </form>;
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function HomePageRender() {
           <h1 className={styles.title}>
             <img src="/public/OpenCoreCMSIcon-500.png" height="120" alt="Open Core CMS logo" />
             <br />
-            Open Core CMS Demo
+            <span className={styles.brandName}>Open Core CMS Demo</span>
           </h1>
 
           <SearchInputJumbotron></SearchInputJumbotron>
@@ -19,13 +19,21 @@ export default function HomePageRender() {
 
           <div className={styles.grid}>
             <a href="/journals" className={styles.card}>
-              <h2>Published Journals &rsaquo;</h2>
-              <p>See example journals published on this demo site.</p>
+              <h2>Browse Journals &rsaquo;</h2>
+              <p>Discover the journals published on this demo site</p>
+            </a>
+
+            <a href="/subjects" className={styles.card}>
+              <h2>Browse subjects &rsaquo;</h2>
+              <p>Explore the range of subjects published on this demo site</p>
             </a>
 
             <a href="/help/about" className={styles.card}>
               <h2>Learn more &rsaquo;</h2>
-              <p>Learn more about Open Core CMS</p>
+              <p>
+                Open Core CMS can be a great fit for&nbsp;
+                <a href="#">publishers</a> and <a href="#">content creators</a>
+              </p>
             </a>
 
           </div>
